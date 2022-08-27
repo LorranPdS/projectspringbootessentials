7ae37431292576342151b472b145840810e79518
LorranPdS <lorransantospereira@yahoo.com.br>
2022-08-27 02:40:24 -0300

    class22 - overwriting handler on spring

--- src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
+++ src/main/java/academy/devdojo/springboot2/handler/RestExceptionHandler.java
@@ -2,30 +2,41 @@ package academy.devdojo.springboot2.handler;
 
 import academy.devdojo.springboot2.exception.BadRequestException;
 import academy.devdojo.springboot2.exception.BadRequestExceptionDetails;
+import academy.devdojo.springboot2.exception.ExceptionDetails;
 import academy.devdojo.springboot2.exception.ValidationExceptionDetails;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /*
-Quando você usa a anotação ControllerAdvice, você está dizendo para todos os
-controllers que eles devem utilizar o que você for colocar dentro dessa classe
-aqui baseado como se fosse numa Flag. A Flag que iremos utilizar é a @ExceptionHandler
+1) Vamos padronizar no handler também a mensagem das requisições quando vier algum
+tipo de erro. Primeiro ponto então foi estender a classe ResponseEntityExceptionHandler
+e simulamos com a chamada do Postman "http://localhost:8080/animes?trace=true" e no
+body do Postman faltando uma aspa para causar o erro.
+
+2) Ao entrar na classe que estendemos, você verá um tipo de tratamento justamente
+para esse tipo de erro dito no retorno que deu do Postman. Esse método com o tratamento
+nós iremos copiar e fazer algumas modificações (será o 3)). Ao entrar nela, lembre-se
+de clicar na opção "download resources"
  */
 @ControllerAdvice
 @Log4j2
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre){
+    public ResponseEntity<BadRequestExceptionDetails> handleBadRequestException(BadRequestException bre){
         return new ResponseEntity<>(
                 BadRequestExceptionDetails.builder()
                         .timestamp(LocalDateTime.now())
@@ -36,9 +47,15 @@ public class RestExceptionHandler {
                         .build(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception){
+    /*
+    4) Trocamos também o tratamento de erro desse método aqui aos moldes do que fizemos
+    no tratamento do erro do item 3) haja vista já existir um modelo parecido. Foram
+    feitas algumas modificações na assinatura do método apenas (seria a parte que tem
+    até as chaves até iniciar o bloco do código)
+     */
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
 
@@ -55,5 +72,24 @@ public class RestExceptionHandler {
                         .fields(fields)
                         .fieldsMessage(fieldsMessage)
                         .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    /*
+    3) Logo abaixo temos o método que foi copiado, conforme dito no item 2), originalmente
+    com o nome "handleExceptionInternal"
+     */
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .title(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, headers, status);
     }
 }
