b80c4cd130d6bb70bbdc758a7d12b50de80dc078
LorranPdS <lorransantospereira@yahoo.com.br>
2022-08-29 22:47:11 -0300

    cleanning comments

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -12,10 +12,7 @@ import java.util.List;
 @Log4j2
 public class SpringClient {
     public static void main(String[] args) {
-        /*
-        Basicamente a diferença entre o "getForEntity" e o "getForObject" dos dois retornos abaixo é que
-        o primeiro (entity) retornará o objeto dentro do response, enquanto o segundo (object) retornará o próprio objeto.
-        */
+
         ResponseEntity<Anime> entity = new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 6);
         log.info(entity);
 
@@ -31,21 +28,10 @@ public class SpringClient {
                 new ParameterizedTypeReference<>(){});
         log.info(exchange.getBody());
 
-        /* Fazendo o RestTemplate POST
-        1) Com o RestTemplate usando o método POST funciona de uma forma bem parecida com o que houve usando o RestTemplate do GET,
-        como veremos ocorrer com as variáveis kingdomSaved e samudaiChamplooSaved, uma usando o "postForObject" e outra usando
-        o "exchange"
-         */
         Anime kingdom = Anime.builder().name("Kingdom").build();
         Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes/", kingdom, Anime.class);
         log.info("Saved anime {}", kingdomSaved);
 
-        /*
-        2) Uma pequena diferença do "exchange POST" abaixo para o "exchange GET" acima é que acima há um argumento null porque o
-        nosso RequestEntity não usa uma requisição GET para argumentação, mas como no POST nós precisamos enviar um objeto, nós
-        passamos o objeto "samuraiChamploo" dentro do HttpEntity.
-        Portanto, uma coisa boa em se usar o "exchange" é que você pode enviar headers HTTP dentro do HttpEntity.
-         */
         Anime samuraiChamploo = Anime.builder().name("samuraiChamploo").build();
         ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
                         HttpMethod.POST,
@@ -54,10 +40,6 @@ public class SpringClient {
         log.info("Saved anime {}", samuraiChamplooSaved);
     }
 
-    /*
-    3) Através desse método abaixo nós iremos enviar um header dizendo que o ContentType dessa
-    requisição é um application/json
-     */
     private static HttpHeaders createJsonHeader(){
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
