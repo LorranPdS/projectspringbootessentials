98aae60e6eb778b0bec9d04722054bdeb68a8916
LorranPdS <lorransantospereira@yahoo.com.br>
2022-09-02 17:24:19 -0300

    cleaning comments

--- src/test/java/academy/devdojo/springboot2/repository/AnimeRepositoryTest.java
+++ src/test/java/academy/devdojo/springboot2/repository/AnimeRepositoryTest.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @DataJpaTest
 @DisplayName("Tests for Anime Repository")
-@Log4j2 // só colocamos o log4j2 para ver se o dado está correto mesmo
+@Log4j2
 class AnimeRepositoryTest {
 
     @Autowired
@@ -24,6 +24,7 @@ class AnimeRepositoryTest {
     void save_PersistAnime_WhenSuccessful(){
 
         Anime animeToBeSaved = createAnime();
+
         Anime animeSaved = this.animeRepository.save(animeToBeSaved);
 
         Assertions.assertThat(animeSaved).isNotNull();
@@ -37,21 +38,12 @@ class AnimeRepositoryTest {
 
         Anime animeToBeSaved = createAnime();
         Anime animeSaved = this.animeRepository.save(animeToBeSaved);
-
-        // 1) Após, salvo, então vamos setar um outro nome para atualizar o conteúdo
         animeSaved.setName("Overlord");
 
-        // 2) Atualizamos o que foi setado acima na base de dados e a recuperamos numa variável local para fazer as asserções
         Anime animeUpdated = this.animeRepository.save(animeSaved);
-//        log.info(animeUpdated.getName()); => já podemos remover esse cara
 
-        // Asserção 1: garantir que o retorno do que foi salvo acima não está nulo
         Assertions.assertThat(animeUpdated).isNotNull();
-
-        // Asserção 2: garantir que o id retornado foi gerado na h2
         Assertions.assertThat(animeUpdated.getId()).isNotNull();
-
-        // Asserção 3: verificar se o nome com o qual foi setado é igual ao que está na h2 (Overlord)
         Assertions.assertThat(animeUpdated.getName()).isEqualTo(animeSaved.getName());
     }
 
@@ -59,19 +51,12 @@ class AnimeRepositoryTest {
     @DisplayName("Delete removes anime when sucessful")
     void delete_RemovesAnime_WhenSuccessful(){
 
-        // 1) Aqui o anime foi criado no objeto
         Anime animeToBeSaved = createAnime();
-
-        // 2) Abaixo ele será salvo no h2
         Anime animeSaved = this.animeRepository.save(animeToBeSaved);
 
-        // 3) Precisamos receber o retorno para fazer a asserção mas veja que o retorno do delete é void, então faremos conforme o próximo item
         this.animeRepository.delete(animeSaved);
-
-        // 4) Desta forma que testaremos mesmo o delete sendo void: verificando se o id no anime criado num primeiro momento ainda existe no h2, então se ele não existe, significa que foi removido
         Optional<Anime> animeOptional = this.animeRepository.findById(animeSaved.getId());
 
-        // Asserção: se o retorno da busca vier vazio, sabemos que foi sim removido do h2
         Assertions.assertThat(animeOptional).isEmpty();
     }
 
@@ -79,19 +64,12 @@ class AnimeRepositoryTest {
     @DisplayName("Find by name returns list of anime when sucessful")
     void findByName_ReturnsListOfAnime_WhenSuccessful(){
 
-        // 1) Aqui o anime foi criado no objeto
         Anime animeToBeSaved = createAnime();
-
-        // 2) Abaixo ele será salvo no h2
         Anime animeSaved = this.animeRepository.save(animeToBeSaved);
 
-        // 3) A seguir vou fazer a busca do anime pelo nome para ver se o findByName está funcionando e o mesmo me retornará uma lista de animes
         List<Anime> animes = this.animeRepository.findByName(animeSaved.getName());
 
-        // Asserção 1: verificar se a lista acima não veio vazia. Desta forma sabemos que o nome foi buscado
         Assertions.assertThat(animes).isNotEmpty();
-
-        // Asserção 2: verificar se na lista de animes retornada há o anime que foi salvo no h2 (item 2)
         Assertions.assertThat(animes).contains(animeSaved);
     }
 
@@ -99,11 +77,9 @@ class AnimeRepositoryTest {
     @DisplayName("Find by name returns empty list when no anime is found")
     void findByName_ReturnsEmptyList_WhenAnimeIsNotFound(){
 
-        // 1) Vamos fazer uma busca por um nome que sabemos não existir e testar a reação dele. No nosso caso, ele virá null
         List<Anime> animes = this.animeRepository.findByName("This name isn't exists");
 
-        // Asserção: verificar se realmente o nome do anime que coloquei virá vazio na lista, já que coloquei um nome que não existe
-        Assertions.assertThat(animes).isEmpty(); // Obs.: aqui, o '.isEmpty()' é diferente de '.isNull()'
+        Assertions.assertThat(animes).isEmpty();
     }
 
     private Anime createAnime(){
